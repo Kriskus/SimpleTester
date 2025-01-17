96819c086601e2a2cb36a3943a7dc8571b67a8ad
Krzysztof Kuśmierczyk <k.kusmierczyk@posnet.com>
2025-01-17 12:54:13 +0100

    Move connection UI to MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,15 +7,14 @@
 
 #include "task/taskoperatons.h"
 
-
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
-    , m_cfgWindow(new CommunicationSettings(this))
+    , m_serial(new SerialPort(this))
 {
     qRegisterMetaType<QTextCursor>("QTextCursor");
     configureSettings();
+    getPortsAvailable();
 }
 
 MainWindow::~MainWindow()
@@ -30,23 +29,46 @@ void MainWindow::readDataFile()
     QScopedPointer<JsonReader> json(new JsonReader(filePath));
     m_sequences = json->loadSequencesFromJson();
     ui->pushButtonStart->show();
-    ui->label->show();
 }
 
 void MainWindow::configureSettings()
 {
     ui->setupUi(this);
     connect(ui->pushButtonChooseFile, &QPushButton::clicked, this, &MainWindow::readDataFile);
-    connect(ui->pushButtonConfig, &QPushButton::clicked, m_cfgWindow.data(), &CommunicationSettings::show);
-
     connect(ui->pushButtonStart, &QPushButton::clicked, this, &MainWindow::startThread);
     connect(ui->pushButtonClose, &QPushButton::clicked, this, &MainWindow::close);
+    connect(ui->pushButtonConfig, &QPushButton::clicked, [this]()
+    {
+        if(ui->groupBoxSettings->isVisible()) {
+            ui->groupBoxSettings->hide();
+        } else {
+            ui->groupBoxSettings->show();
+        }
+    });
 
-    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::resumeTask);
+    connect(ui->pushButtonConnect, &QPushButton::clicked, this, &MainWindow::connectToDevice);
+    connect(ui->pushButtonDisconnect, &QPushButton::clicked, this, &MainWindow::disconnectFromDevice);
+    connect(ui->pushButtonRefresh, &QPushButton::clicked, this, &MainWindow::getPortsAvailable);
+    connect(m_serial.data(), &SerialPort::sendInformation, this, &MainWindow::showStatus);
 
+    connect(m_serial.data(), &SerialPort::dataSended, this, &MainWindow::dataSended);
+    connect(m_serial.data(), &SerialPort::sendDataReceived, this, &MainWindow::sendDataReceived);
+
+    connect(this, &MainWindow::sendSequenceToCom, m_serial.data(), &SerialPort::deviceWrite);
+
+
+    ui->groupBoxSettings->hide();
     ui->pushButtonStart->hide();
     ui->pushButtonStop->hide();
-    ui->label->hide();
+    ui->pushButtonDisconnect->hide();
+}
+
+void MainWindow::showStatus(bool status, const QString &information)
+{
+    m_isConnectionActive = status;
+    ui->pushButtonConnect->setVisible(!m_isConnectionActive);
+    ui->pushButtonDisconnect->setVisible(m_isConnectionActive);
+    ui->statusbar->showMessage(information, 5000);
 }
 
 void MainWindow::startThread()
@@ -60,18 +82,77 @@ void MainWindow::startThread()
     task->moveToThread(threadTask);
 
     connect(threadTask, &QThread::started, task, &TaskOperatons::startTask);
-
-    connect(task, &TaskOperatons::sendSequence, m_cfgWindow.data(), &CommunicationSettings::getSequenceToSend);
-    connect(m_cfgWindow.data(), &CommunicationSettings::sendDataReceived, this, &MainWindow::getResponseFromDevice);
     connect(task, &TaskOperatons::sendResponseAwaited, this, &MainWindow::getAwaitedResponse, Qt::DirectConnection);
-
+    connect(task, &TaskOperatons::sendSequenceName, ui->textBrowserInformation, &QTextBrowser::append);
+    connect(task, &TaskOperatons::sendTestCaseName, ui->textBrowserInformation, &QTextBrowser::append);
     connect(this, &MainWindow::resumeTask, task, &TaskOperatons::resumeTask, Qt::DirectConnection);
+
+    connect(task, &TaskOperatons::sendSequence, this, &MainWindow::getSequenceToSend);
+    connect(this, &MainWindow::sendDataReceived, this, &MainWindow::getResponseFromDevice);
 
     connect(task, &TaskOperatons::finished, threadTask, &QThread::quit);
     connect(threadTask, &QThread::finished, threadTask, &QThread::deleteLater);
     connect(task, &TaskOperatons::finished, task, &TaskOperatons::deleteLater);
 
     threadTask->start();
+}
+
+void MainWindow::connectToDevice()
+{
+    if(ui->radioButtonCOM->isChecked()) {
+        m_serial->deviceConnect(ui->comboBoxPortName->currentText(),
+                                ui->comboBoxPortSpeed->currentText().toInt(),
+                                ui->comboBoxParity->currentIndex(),
+                                ui->comboBoxFlowControl->currentIndex(),
+                                ui->comboBoxDataBits->currentText().toInt(),
+                                ui->comboBoxStopBits->currentText().toInt()
+                                );
+    } else {
+
+    }
+}
+
+void MainWindow::disconnectFromDevice()
+{
+    if(ui->radioButtonCOM->isChecked()) {
+        m_serial->deviceDisconnect();
+    } else {
+
+    }
+}
+
+void MainWindow::getPortsAvailable()
+{
+    if(m_isFirstPortsRead) {
+        connect(ui->comboBoxPortName, QOverload<int>::of(&QComboBox::currentIndexChanged), [&](int index) {
+                QString tooltip = ui->comboBoxPortName->itemData(index, Qt::ToolTipRole).toString();
+                ui->comboBoxPortName->setToolTip(tooltip);
+            });
+        m_isFirstPortsRead = false;
+    }
+    ui->comboBoxPortName->clear();
+    const auto serialPortInfos = QSerialPortInfo::availablePorts();
+    for(auto& el: serialPortInfos) {
+        ui->comboBoxPortName->addItem(el.portName());
+        ui->comboBoxPortName->setItemData(ui->comboBoxPortName->count()-1, el.description() + " | " + el.manufacturer(), Qt::ToolTipRole);
+    }
+    ui->comboBoxPortName->setToolTip(ui->comboBoxPortName->itemData(0, Qt::ToolTipRole).toString());
+}
+
+void MainWindow::getSequenceToSend(const QString &sequence)
+{
+    if(m_isConnectionActive) {
+        if(ui->radioButtonCOM->isChecked()) {
+            emit sendSequenceToCom(convertSequenceToHex(sequence));
+        } else {
+            emit sendSequenceToSocket(convertSequenceToHex(sequence));
+        }
+    }
+}
+
+QByteArray MainWindow::convertSequenceToHex(const QString &sequence)
+{
+    return QByteArray::fromHex(sequence.toLatin1());
 }
 
 void MainWindow::getResponseFromDevice(const QByteArray &responseReceived)
@@ -89,12 +170,20 @@ void MainWindow::checkResponses(const QByteArray &responseReceived)
     QByteArray responseAwaited = m_responseAwaited.replace(" ", "").toUpper().toLatin1();
 
     if(responseAwaited == responseReceived.toHex().toUpper()) {
-        ui->textBrowserInformation->append("<span style='color: green; font: bold;'>Poprawnie</span>");
+        ui->textBrowserInformation->append(tab + tab + "<span style='color: green; font: bold;'>Poprawnie</span>");
+        ui->textBrowserInformation->append("");
     } else {
-        ui->textBrowserInformation->append("<span style='color: red; font: bold;'>Niepoprawnie</span>");
-        ui->textBrowserInformation->append("<span style='color: red; font: italic;'>Oczekiwana: " + responseAwaited + "</span>");
-        ui->textBrowserInformation->append("<span style='color: red; font: italic;'>Otrzymana:  " + responseReceived.toHex().toUpper() + "</span>");
+        ui->textBrowserInformation->append(tab + tab + "<span style='color: red; font: bold;'>\tNiepoprawnie</span>");
+        ui->textBrowserInformation->append(tab + tab + "<span style='color: red; font: italic;'>\tOczekiwana: " + responseAwaited + "</span>");
+        ui->textBrowserInformation->append(tab + tab + "<span style='color: red; font: italic;'>\tOtrzymana:  " + responseReceived.toHex().toUpper() + "</span>");
+        ui->textBrowserInformation->append("");
     }
     emit resumeTask();
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    Q_UNUSED(event);
+    close();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,9 +3,10 @@
 #include <QMainWindow>
 #include <QFileDialog>
 #include <QScopedPointer>
+#include <QSerialPortInfo>
 
 #include "fileOperation/sequencestructure.h"
-#include "comm/communicationsettings.h"
+#include "comm/serialport.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -21,23 +22,39 @@ public:
 
 private:
     Ui::MainWindow *ui;
-    QScopedPointer<CommunicationSettings> m_cfgWindow;
-
+    QScopedPointer<SerialPort> m_serial;
     QMap<QString, Sequence> m_sequences{};
-
     QString m_responseAwaited{};
+    bool m_isConnectionActive;
+    bool m_isFirstPortsRead{true};
+
+    QString tab{"&nbsp;&nbsp;&nbsp;&nbsp;"};
 
 private slots:
     void readDataFile();
     void configureSettings();
+    void showStatus(bool status, const QString &information);
 
     void startThread();
+
+    void connectToDevice();
+    void disconnectFromDevice();
+
+    void getPortsAvailable();
+    void getSequenceToSend(const QString &sequence);
+    QByteArray convertSequenceToHex(const QString &sequence);
 
     void getResponseFromDevice(const QByteArray& responseReceived);
     void getAwaitedResponse(const QString& responseReceived);
     void checkResponses(const QByteArray& responseReceived);
 
+    void closeEvent(QCloseEvent* event);
+
 signals:
     void sendData(const QString&);
     void resumeTask();
+    void dataSended();
+    void sendDataReceived(const QByteArray&);
+    void sendSequenceToCom(const QByteArray&);
+    void sendSequenceToSocket(const QByteArray&);
 };
